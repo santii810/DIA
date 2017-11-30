56b77266a1cff959cfec9d04bc952c375f6ba9c6
santii810 <santii810@hotmail.com>
2017-11-30 22:19:49 +0100

    Primer commit, Iniciando Vista principal

---
+++ GestionPedidos/GestionPedidos/View/MainWindowCore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionPedidos.View
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            this.Build();
+        }
+
+
+        private void Salir()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void Inserta()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Guardar()
+        {
+            throw new NotImplementedException();
+        }
+        private void Eliminar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
