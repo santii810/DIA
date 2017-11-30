4d976fae8312b5ab52eedddd319d9b42cbfbe231
Silvia Rodríguez <silviardguez@gmail.com>
2017-11-30 23:51:43 +0100

    GuardaXml implementado

---
+++ GestionPedidos/GestionPedidos/Core/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPedidos.Core
+{
+    public class Pedido
+    {
+        public string Nombre { get; private set; }
+        public int Cliente { get; private set; }
+        public DateTime Entrega { get; private set; }
+
+        public Pedido(string nombre,  int cliente, DateTime entrega)
+        {
+            Nombre = nombre;
+            Cliente = cliente;
+            Entrega = entrega;
+        }
+    }
+}
--- GestionPedidos/GestionPedidos/Core/RegistroPedidos.cs
+++ GestionPedidos/GestionPedidos/Core/RegistroPedidos.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace GestionPedidos.Core
 {
-    internal class RegistroPedidos
+    public class RegistroPedidos
     {
-        internal void GuardaXml()
+        public const string ArchivoXml = "pedidos.xml";
+        public const string EtqPedidos = "pedidos";
+        public const string EtqPedido = "pedido";
+        public const string EtqEntrega = "fechaEntrega";
+        public const string EtqCliente = "idCliente";
+        public const string EtqIdPedido = "id";
+
+        private List<Pedido> pedidos;
+
+        /// <summary>
+        /// Crea un <see cref="T:GestionPedidos.Core.RegistroPedidos"/> sin pedidos.
+        /// </summary>
+        public RegistroPedidos()
         {
-            throw new NotImplementedException();
+            this.pedidos = new List<Pedido>();
+        }
+
+        /// <summary>
+        /// Inserta un pedido dado al final de la lista.
+        /// </summary>
+        /// <param name="pedido">Un objeto <see cref="Pedido"/>.</param>
+		public void Add(Pedido pedido)
+        {
+            this.pedidos.Add(pedido);
+        }
+
+        public void GuardaXml()
+        {
+            this.GuardaXml(ArchivoXml);
+        }
+
+        /// <summary>
+        /// Guarda la lista de viajes como XML.
+        /// <param name="nf">El nombre del archivo.</param>
+        /// </summary>
+        public void GuardaXml(string nf)
+        {
+            var doc = new XDocument();
+            var raiz = new XElement(EtqPedidos);
+
+            foreach (Pedido pedido in this.pedidos)
+            {
+                raiz.Add(
+                    new XElement(EtqPedido,
+                            new XAttribute(EtqIdPedido, pedido.Nombre),
+                            new XElement(EtqCliente, pedido.Cliente.ToString()),
+                            new XElement(EtqEntrega, pedido.Entrega.ToString())));
+            }
+
+            doc.Add(raiz);
+            doc.Save(nf);
         }
     }
 }
