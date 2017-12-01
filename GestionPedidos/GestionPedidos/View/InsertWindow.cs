2180abba1bea40417b5faee87b4e57cfaa72a1b8
santii810 <santii810@hotmail.com>
2017-12-01 02:52:57 +0100

    Revert "Borrado inserWindow"
    This reverts commit 6d953c73554137978bcb628529a687d98463f770.

---
+++ GestionPedidos/GestionPedidos/View/InsertWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace GestionPedidos.View
+{
+    internal class InsertWindow : Form
+    {
+        private NumericUpDown edCliente;
+
+        public InsertWindow()
+        {
+        }
+    }
+}
